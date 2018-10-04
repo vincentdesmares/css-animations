739b74edb0e56ed027e6b91e74b275deef567cd6
Vincent Desmares <vincent.desmares@gmail.com>
2018-10-04 09:58:08 +0200

    Update

--- src/presentation.js
+++ src/presentation.js
@@ -8,7 +8,8 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  CodePane
 } from "spectacle";
 
 import CodeSlide from "spectacle-code-slide";
@@ -60,6 +61,78 @@ const easeDemo = `.parentHover:hover #ease,
   transform: translateX(0px);
   transition-property: transform;
   transition-duration: 1s;
+}`;
+
+const pathDemo = `.path {
+  stroke-dasharray: 1000;
+  stroke-dashoffset: 1000;
+  animation: dash 5s linear alternate infinite;
+}
+
+@keyframes dash {
+  from {
+    stroke-dashoffset: 1000;
+  }
+  to {
+    stroke-dashoffset: 0;
+  }
+}`;
+
+const keyframesDemo = `.pyro >.before,
+.pyro>.after {
+  position: absolute;
+  z-index: 10;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  box-shadow: 0 0 #f442d9, 0 0 #eef442, 0 0 #f442d9, 0 0 #eef442, 0 0 #f442d9, 0 0 #eef442, 0 0 #f442d9, 0 0 #eef442, 0 0 #f442d9, 0 0 #2ddce5, 0 0 #f442d9, 0 0 #2ddce5, 0 0 #f442d9, 0 0 #2ddce5, 0 0 #f442d9, 0 0 #2ddce5, 0 0 #f442d9, 0 0 #5ff248, 0 0 #f442d9, 0 0 #5ff248, 0 0 #f442d9, 0 0 #5ff248, 0 0 #f442d9, 0 0 #5ff248, 0 0 #f442d9, 0 0 #5ff248, 0 0 #f442d9, 0 0 #f24330, 0 0 #f442d9, 0 0 #f24330, 0 0 #f442d9, 0 0 #f24330, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9, 0 0 #f442d9;
+  animation: 1s bang ease-out infinite backwards, 1s gravity ease-in infinite backwards, 5s position linear infinite backwards;
+}
+
+.pyro>.after {
+  animation-delay: 1.25s, 1.25s, 1.25s;
+  animation-duration: 1.25s, 1.25s, 6.25s;
+}
+
+@keyframes bang {
+  to {
+    box-shadow: 65px -166.66667px #f2ff00, -66px -110.66667px #b300ff, -195px -12.66667px #a6ff00, -229px -141.66667px #0009ff, 191px 80.33333px #55ff00, -188px -415.66667px #00aaff, -194px -276.66667px #c800ff, 39px -175.66667px #ff9900, 110px -64.66667px #ff6200, 202px -297.66667px #ff0026, -203px -28.66667px #00c4ff, 233px -63.66667px #00ddff, -128px -145.66667px #00fff7, -216px -386.66667px #00d0ff, -152px -372.66667px #ff8000, 8px 61.33333px #2bff00, 49px -388.66667px #0066ff, 114px -243.66667px #00e6ff, -224px -382.66667px #003cff, -101px -224.66667px #00d0ff, 92px -255.66667px #51ff00, 207px -272.66667px #2bff00, 225px -244.66667px #00d0ff, -13px -189.66667px #33ff00, 200px -278.66667px #3c00ff, -225px -101.66667px #f700ff, 53px -292.66667px #000dff, 84px -340.66667px #ff1a00, -72px -72.66667px #ff5500, 111px 9.33333px #00ff6a, -206px -74.66667px #04ff00, 98px -14.66667px #77ff00, 241px -106.66667px #00ffa6, 69px -361.66667px #4400ff, 242px 7.33333px #00ff40, -102px -76.66667px #7700ff, 11px -84.66667px #44ff00, -104px -128.66667px #ff7700, -244px -223.66667px #ff00e6, 97px -127.66667px #88ff00, -115px -272.66667px #ff006f, -66px 74.33333px #aa00ff, 125px 65.33333px #00ffee, -202px -239.66667px #bf00ff, -25px -242.66667px #ff2600, -144px -177.66667px #0055ff, 215px -152.66667px #ffb700, 132px -66.66667px #5900ff, 135px -241.66667px #00ff33, -236px 55.33333px #ff5e00, 73px -30.66667px #6fff00;
+  }
+}
+
+@keyframes gravity {
+  to {
+    transform: translateY(200px);
+    opacity: 0;
+  }
+}
+
+@keyframes position {
+  0%,
+  19.9% {
+    margin-top: 10%;
+    margin-left: 40%;
+  }
+  20%,
+  39.9% {
+    margin-top: 40%;
+    margin-left: 30%;
+  }
+  40%,
+  59.9% {
+    margin-top: 20%;
+    margin-left: 70%;
+  }
+  60%,
+  79.9% {
+    margin-top: 30%;
+    margin-left: 20%;
+  }
+  80%,
+  99.9% {
+    margin-top: 30%;
+    margin-left: 80%;
+  }
 }`;
 
 export default class Presentation extends React.Component {
@@ -255,6 +328,7 @@ export default class Presentation extends React.Component {
             }
           ]}
         />
+
         <Slide transition={["fade"]} bgColor="primary">
           <Heading size={6} textColor="tertiary" caps fit>
             What can I animate?
@@ -277,6 +351,237 @@ export default class Presentation extends React.Component {
             <ListItem>rotate</ListItem>
           </List>
         </Slide>
+
+        <Slide transition={["fade"]} bgColor="cinquo">
+          <Heading size={6} textColor="tertiary" caps fit>
+            DEMO TIME
+          </Heading>
+          <Text size={6} textColor="quartenary" margin="20px 0" bold>
+            Transition based animations
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Heading size={6} textColor="tertiary" caps fit>
+            What's dash-strokeoffset?
+          </Heading>
+          <Text size={6} textColor="quartenary" margin="20px 0" bold>
+            It allows you, in addition with stroke-dasharray, to draw on the
+            paths of a svg.
+          </Text>
+          <style>{pathDemo}</style>
+          <svg
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="340px"
+            height="333px"
+            viewBox="0 0 340 333"
+            enableBackground="new 0 0 340 333"
+            xmlSpace="preserve"
+          >
+            <path
+              class="path"
+              fill="#000000"
+              stroke="#ff00ac"
+              strokeWidth="4"
+              strokeMiterlimit="10"
+              d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
+    s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
+    C46.039,146.545,53.039,128.545,66.039,133.545z"
+            />
+          </svg>
+          <Text textColor="primary">
+            Checkout the intro of http://animejs.com/ and http://mojs.io/
+          </Text>
+        </Slide>
+        <CodeSlide
+          transition={[]}
+          lang="html"
+          code={pathDemo}
+          bgColor={"#1F2022"}
+          color={"#FFFFFF"}
+          ranges={[
+            { loc: [0, 15], title: "HOW IT WORKS" },
+            {
+              loc: [1, 4],
+              note: "Lonnnng dashes + moving offset = awesome animations"
+            },
+            {
+              loc: [6, 13],
+              note: "What's keyframes and animations?"
+            }
+          ]}
+        />
+
+        <Slide
+          transition={["fade"]}
+          bgColor="primary"
+          progressColor="secondary"
+          controlColor="secondary"
+        >
+          <style>{keyframesDemo}</style>
+          <div className="pyro">
+            <div className="before" />
+            <div className="after" />
+            <Heading size={6} textColor="tertiary" caps>
+              Keyframes let you do multiple step animations.
+            </Heading>
+            <style>{cssWithAnimations}</style>
+
+            <Text margin="10px 0 0" textColor="secondary" size={0.5}>
+              It can be pretty awesome
+            </Text>
+          </div>
+        </Slide>
+
+        <CodeSlide
+          transition={[]}
+          lang="html"
+          code={keyframesDemo}
+          bgColor={"#1F2022"}
+          color={"#FFFFFF"}
+          ranges={[
+            { loc: [0, 15], title: "HOW IT WORKS" },
+
+            {
+              loc: [29, 60],
+              note: "You define your animation(s)"
+            },
+            {
+              loc: [30, 35],
+              note: "And you define each step"
+            },
+            {
+              loc: [35, 40]
+            },
+            {
+              loc: [40, 45]
+            },
+            {
+              loc: [45, 50]
+            },
+            {
+              loc: [50, 55]
+            },
+            {
+              loc: [8, 9],
+              note: "You pick your animation(s), with their easing and all."
+            }
+          ]}
+        />
+
+        <Slide transition={["fade"]} bgColor="cinquo">
+          <Heading size={6} textColor="tertiary" caps fit>
+            Most used transition in TS
+          </Heading>
+          <Text
+            size={6}
+            textColor="quartenary"
+            margin="20px 0"
+            textAlign="left"
+          >
+            <CodePane
+              lang="css"
+              source={`const styles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 1 },
+  exited: { opacity: 0 }
+}`}
+            />
+            <CodePane
+              lang="css"
+              source={`const styles = {
+  entering: { opacity: 0, transform: 'scale(0.3)' },
+  entered: { opacity: 1, transform: 'scale(1)' },
+  exiting: { opacity: 1 },
+  exited: { display: 'none', opacity: 0 }
+}`}
+            />
+            <CodePane
+              lang="css"
+              source={`const styles = {
+  entering: { opacity: 0, transform: 'translateX(200px)' },
+  entered: { opacity: 1, transform: 'translateX(0px)' },
+  exiting: { opacity: 0, transform: 'translateX(-200px)' },
+  exited: { display: 'none', opacity: 0, transform: 'translateX(-200px)' }
+}`}
+            />
+          </Text>
+          <Text size={6} textColor="quartenary" margin="20px 0" bold>
+            Appear / Pop / Appear from right, fade to left
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={1} textColor="tertiary" caps fit>
+            A quick React integration
+          </Heading>
+          <Text
+            size={6}
+            textColor="quartenary"
+            margin="20px 0"
+            textAlign="left"
+          />
+          <CodePane
+            lang="javascript"
+            source={`import React from 'react'
+import Transition from 'react-transition-group/Transition'
+
+const styles = {/* from previous slide */}
+
+export default ({loading}) => (
+  <Transition
+    in={!loading}
+    timeout={{
+      enter: 10,
+      exit: 300
+    }}
+  >
+    {state => (
+      <div
+        className="my classes"
+        style={{
+          height: 600,
+          transition: "opacity 0.3s ease-out, transform 0.5s ease-out",
+          ...styles[state]
+        }}
+      >
+        My content
+      </div>
+    )}
+  </Transition>
+)`}
+          />
+          <Text size={6} textColor="quartenary" margin="20px 0" bold>
+            Animations rely on state-machines.
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="cinquo">
+          <Heading size={6} textColor="tertiary" caps fit>
+            DEMO TIME (Again)
+          </Heading>
+          <Text size={6} textColor="quartenary" margin="20px 0" bold>
+            Form animations
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+          <Heading size={6} textColor="tertiary" caps>
+            Some performance tips:
+          </Heading>
+          <List>
+            <ListItem>Do not animate width/height/left/right</ListItem>
+            <ListItem>Animate transformX/transformY</ListItem>
+            <ListItem>More generaly: Avoid reflows!</ListItem>
+          </List>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="cinquo" textColor="secondary">
           <Heading size={6} textColor="quartenary" caps>
             Some Debug tips:
@@ -285,17 +590,30 @@ export default class Presentation extends React.Component {
             <ListItem>Use "force state" -> ":hover"</ListItem>
             <ListItem>Use "More tools" -> "Animations" (CR/FF)</ListItem>
             <ListItem>"transform" works for a CHANGE</ListItem>
+            <ListItem>You can't transform "display"</ListItem>
             <ListItem>use units for functions (px/em/ms/s)</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
-          <Heading size={6} textColor="tertiary" caps>
-            Some performance tips:
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading
+            size={6}
+            textColor="quartenary"
+            caps
+            fit
+            margin="100px 0 0 0"
+          >
+            Questions?
           </Heading>
-          <List>
-            <ListItem>Do not animate width/height/left/right</ListItem>
-            <ListItem>Animate transformX/transformY</ListItem>
-          </List>
+          <Text>
+            Repo:{" "}
+            <a
+              className="dib"
+              style={{ margin: 30 }}
+              href="https://github.com/vincentdesmares/css-animations"
+            >
+              https://github.com/vincentdesmares/css-animations
+            </a>
+          </Text>
         </Slide>
       </Deck>
     );
